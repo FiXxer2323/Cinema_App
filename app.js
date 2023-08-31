be442ac1b98fc17366981dfc54eceaf0fbeaf19e
FiXxer2323 <kabarcz.kristof23@gmail.com>
2023-08-31 21:14:12 +0200

    Slider Posters

--- app.js
+++ app.js
@@ -11,4 +11,28 @@ const mozik = [
     },
 ]
 
+const slider = document.querySelector('.slider-container');
+const slides = document.querySelectorAll('.slider-container article');
+const interval = 3000;
+
+let curr = 0;
+let prev = 0;
+
+function showSlide() {
+  if (curr == 0 || curr == slides.length) {
+    curr = 0;
+    prev = slides.length-1;
+  } else {
+    prev = curr-1;
+  }
+
+  slides[prev].classList.remove('active'); // Előző kép inaktív
+  slides[curr].classList.add('active'); // Új kép aktív
+
+  curr++;
+
+}
+
+showSlide();
+setInterval(showSlide, interval);
 
