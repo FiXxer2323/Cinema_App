65172d77e2296f557a641be1bcfcdb8ed114229c
FiXxer2323 <kabarcz.kristof23@gmail.com>
2023-08-31 20:56:20 +0200

    change

---
+++ app.js
@@ -0,0 +1,14 @@
+const mozik = [
+    {
+        id: 1, 
+        nev: 'Cinema City Arena',
+        cim: 'Széchenyi utca 62',
+    },
+    {
+        id: 2, 
+        nev: 'Cinema City Allee',
+        cim: 'Kelenföldi út 23',
+    },
+]
+
+
